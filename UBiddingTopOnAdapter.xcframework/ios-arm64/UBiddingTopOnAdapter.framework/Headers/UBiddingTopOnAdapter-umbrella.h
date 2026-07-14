a910ce4ad9a5e68fcdc530ef0892e648ec0c34d4
guoqiang <guoqiang.zhu@ubixai.com>
2026-07-14 15:05:48 +0800

    update UBiddingTopOnAdapter 6.5.42.2

---
+++ UBiddingTopOnAdapter.xcframework/ios-arm64/UBiddingTopOnAdapter.framework/Headers/UBiddingTopOnAdapter-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UBiddingTopOnAdapter.h"
+
+FOUNDATION_EXPORT double UBiddingTopOnAdapterVersionNumber;
+FOUNDATION_EXPORT const unsigned char UBiddingTopOnAdapterVersionString[];
+
---
+++ UBiddingTopOnAdapter.xcframework/ios-arm64/UBiddingTopOnAdapter.framework/Headers/UBiddingTopOnAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiddingTopOnAdapter.h
+//  UBiddingTopOnAdapter
+//
+//  Created by guoqiang.zhu on 2024/6/19.
+//  Copyright © 2024 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiddingTopOnAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiddingTopOnAdapter.
+FOUNDATION_EXPORT double UBiddingTopOnAdapterVersionNumber;
+
+//! Project version string for UBiddingTopOnAdapter.
+FOUNDATION_EXPORT const unsigned char UBiddingTopOnAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
